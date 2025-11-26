9f732098d9e55bef62864818955c619f1c401550
hannah896 <phn2260@naver.com>
2025-11-27 01:21:35 +0900

    [Feat] "정답씬 돌아다니는 아저씨 추가"

--- Assets/02.Scripts/GameLogic/Entity/WalkProfsor/Professor.cs
+++ Assets/02.Scripts/GameLogic/Entity/WalkProfsor/Professor.cs
@@ -1,17 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Splines;
 
 public class Professor : MonoBehaviour
 {
-    [SerializeField] SplineContainer splineContainer;
+    [SerializeField] private SplineContainer splineContainer;
+    [SerializeField] private Rigidbody rb;
+
+    private List<Vector3> pos = new();
+    float speed = 1.0f;
+
+    private Vector3 TargetPosition;
+
+
+    private void OnValidate()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Start()
     {
-        //transform.position = splineContainer.Spline.Knots[0].Position;
+        foreach (var point in splineContainer.Spline)
+            pos.Add(point.Position);
+
+        TargetPosition = transform.TransformPoint(pos[0]);
+        StartCoroutine(ChangeTarget());
     }
-    private void Update()
+
+    private void FixedUpdate()
     {
-        float moveSpeed = 2f;
-        float step = moveSpeed * Time.deltaTime;
+        Vector3 move = Vector3.MoveTowards(transform.position, TargetPosition, Time.fixedDeltaTime * speed);
+        rb.MovePosition(move);
+    }
+
+    IEnumerator ChangeTarget()
+    {
+        int idx = 1;
+
+        while (true)
+        {
+            if (idx == splineContainer.Spline.Count)
+                yield break;
+            if (Vector3.Distance(transform.position, TargetPosition) < 0.05f)
+            {
+                idx++;
+                TargetPosition = transform.TransformPoint(pos[idx]);
+            }
+            yield return null;
+        }
     }
 }
--- Assets/02.Scripts/GameLogic/Game/Door.cs
+++ Assets/02.Scripts/GameLogic/Game/Door.cs
@@ -10,18 +10,17 @@ public class Door : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player")|| collision.gameObject.CompareTag("Professor"))
         {
             Util.Log("내가 문열어줄게!!!!!!!");
-            if (collision.transform.position.x > transform.position.x)
+            if (collision.transform.position.x < transform.position.x)
             {
                 rb.DORotate((int)pivot * Vector3.down, 3.5f).SetEase(Ease.OutElastic);
             }
             else
             {
                 rb.DORotate((int)pivot * Vector3.up, 3.5f).SetEase(Ease.OutElastic);
-            }
-
+            }   
         }
     }
 }
