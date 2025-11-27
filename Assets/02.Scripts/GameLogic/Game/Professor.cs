b3789eaacf4c463b822f0f6df01e05d8f3ffbdb1
hannah896 <phn2260@naver.com>
2025-11-27 23:09:37 +0900

    [Feat] "Trap8 구현 완료"

---
+++ Assets/02.Scripts/GameLogic/Game/Professor.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Professor : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private void OnValidate()
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        if (Managers.Game.Current.scene == Managers.Scene.Trap8Scene)
+            animator.SetBool("isTrap8", true);
+        else
+        {
+            animator.SetBool("isTrap8", false);
+        }
+    }
+}
