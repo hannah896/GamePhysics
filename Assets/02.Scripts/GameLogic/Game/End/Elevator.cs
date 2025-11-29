e7eae12ff209e5d5b9027193c7a8b7a8d6fa9f3a
hannah896 <phn2260@naver.com>
2025-11-29 23:55:29 +0900

    [Feat] "엔딩씬 구현완"

--- Assets/02.Scripts/GameLogic/Game/End/Elevator.cs
+++ Assets/02.Scripts/GameLogic/Game/End/Elevator.cs
@@ -6,22 +6,40 @@ public class Elevator : MonoBehaviour
 {
     public Vector3 halfExtents = Vector3.one;
     public float distance = 3f;
-    public LayerMask layerMask = LayerMask.GetMask("Player");
+    public LayerMask layerMask;
 
     public Animator animator;
 
     private void Update()
     {
-        if (Physics.BoxCast(transform.position + Vector3.one, halfExtents, transform.forward, out RaycastHit hit, transform.rotation, distance, layerMask))
+        if (Physics.BoxCast(transform.position, halfExtents, transform.forward, out RaycastHit hit))
         {
-            Managers.Scene.ChangeState(Managers.Scene.EndingCreditScene);
+            if (hit.collider.CompareTag("Player"))
+                Managers.Scene.ChangeState(Managers.Scene.EndingCreditScene);
         }
 
 
-        if (Physics.BoxCast(transform.position + transform.forward * distance, halfExtents, transform.forward, out RaycastHit h, transform.rotation, distance, layerMask))
+        if (Physics.BoxCast(transform.position + transform.forward * distance, halfExtents, transform.forward, out RaycastHit h))
         {
             if (h.collider.CompareTag("Player"))
                 animator.enabled = true;
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 origin = transform.position;
+        Vector3 direction = transform.forward;
+        Quaternion rot = transform.rotation;
+
+        Gizmos.color = Color.red;
+        Gizmos.matrix = Matrix4x4.TRS(origin, rot, Vector3.one);
+        Gizmos.DrawWireCube(Vector3.zero, halfExtents * 2);
+
+        Vector3 end = origin + direction * distance;
+        Gizmos.color = Color.blue;
+        Gizmos.matrix = Matrix4x4.TRS(end, rot, Vector3.one);
+        Gizmos.DrawWireCube(Vector3.zero, halfExtents * 2);
+    }
+
 }
